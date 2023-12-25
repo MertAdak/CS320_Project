a80a7b9802237cb9943891e02b6c92f9fe72e9cb
berkadak <berk.adak@ozu.edu.tr>
2023-12-26 00:00:02 +0300

    feat: add ESP32-CAM camera configuration

--- gotcha.ino
+++ gotcha.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
+#include "esp_camera.h"
 
 const char* ssid = "wifi_name";
 const char* password = "wifi_password";
@@ -26,4 +27,23 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+//ESP32-CAM camera configuration
+void configInitCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5; config.pin_d1 = 18;
+  config.pin_d2 = 19; config.pin_d3 = 21;
+  config.pin_d4 = 36; config.pin_d5 = 39;
+  config.pin_d6 = 34; config.pin_d7 = 35;
+  config.pin_xclk = 0; config.pin_pclk = 22;
+  config.pin_vsync = 25; config.pin_href = 23;
+  config.pin_sscb_sda = 26; config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32; config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 }
